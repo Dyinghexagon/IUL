f024a55b38616dcc7069a0b668e04a884411a6b8
Dyinghexagon <dyinghexagon@gmail.com>
2021-10-26 15:22:15 +0300

    26.10.21

--- CreateIUL.Designer.cs
+++ CreateIUL.Designer.cs
@@ -30,49 +30,20 @@ namespace IUL
         private void InitializeComponent()
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 499);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Выбор папки";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 15);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Имена файлов";
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 35);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(766, 458);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(107, 499);
+            this.button2.Location = new System.Drawing.Point(12, 56);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 30);
             this.button2.TabIndex = 4;
@@ -82,7 +53,7 @@ namespace IUL
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(338, 499);
+            this.textBox1.Location = new System.Drawing.Point(243, 56);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(141, 23);
             this.textBox1.TabIndex = 5;
@@ -90,23 +61,28 @@ namespace IUL
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(231, 501);
+            this.label2.Location = new System.Drawing.Point(136, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(101, 15);
             this.label2.TabIndex = 6;
             this.label2.Text = "Дата подписания";
             // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(298, 138);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
             // CreateIUL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 541);
+            this.ClientSize = new System.Drawing.Size(648, 288);
+            this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "CreateIUL";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -117,13 +93,11 @@ namespace IUL
         #endregion
 
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
     }
 }
 
---
+++ IULs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IUL
+{
+    class IULs
+    {
+        private string _MD5;
+        private DateTime _dateChange;
+        private int _sizeFile;
+        private string _GIP;
+        private string _NKontr;
+        /// <summary>
+        /// Словарь _chapters хранит информацию об том или ином разделе.
+        /// Ключом выступает код раздела.
+        /// Значением выступает объект класса IUL.
+        /// </summary>
+        private Dictionary<string, IUL> _chapters;
+        private DateTime _dateSign;
+    }
+}
