d5a602dac07acb91fd1d3e017af1e812490f804b
Dyinghexagon <dyinghexagon@gmail.com>
2021-11-05 10:37:15 +0300

    05.11.21

--- SQL_SCRIPTS/create_table_engineers.sql
+++
@@ -1,7 +0,0 @@
-use IUL;
-create table Engineers (
-	Id integer auto_increment primary key,
-    Surname varchar(30),
-    Code_id varchar(100),
-    Work_role varchar(30)
-);
--- SQL_SCRIPTS/create_table_project_composition.sql
+++
@@ -1,5 +0,0 @@
-USE IUL;
-create table project_composition (
-	code_id varchar(100) primary key,
-    name_doc varchar(3000)
-);
